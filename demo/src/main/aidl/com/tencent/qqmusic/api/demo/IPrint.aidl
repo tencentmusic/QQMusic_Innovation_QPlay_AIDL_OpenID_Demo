ba049451fb3b872d71806d6a205ffdaa7db8b2cd
clydeazhang <clydeazhang@tencent.com>
2021-05-11 20:15:28 +0800

    pcm播放逻辑放到service
    (cherry picked from commit 9b71137e7a71d933d23b714e7036f8784de70885)

---
+++ demo/src/main/aidl/com/tencent/qqmusic/api/demo/IPrint.aidl
@@ -0,0 +1,8 @@
+// IPrintMessage.aidl
+package com.tencent.qqmusic.api.demo;
+
+// Declare any non-default types here with import statements
+
+interface IPrint {
+    void print(String msg);
+}
--- demo/src/main/java/com/tencent/qqmusic/api/demo/pcm/PlayService.kt
+++ demo/src/main/java/com/tencent/qqmusic/api/demo/pcm/PlayService.kt
@@ -1,39 +1,36 @@
 package com.tencent.qqmusic.api.demo.pcm
 
+import android.app.Service
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.media.AudioFormat
 import android.os.Bundle
+import android.os.Handler
+import android.os.IBinder
 import android.os.ParcelFileDescriptor
-import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
-import com.tencent.qqmusic.api.demo.Config
-import com.tencent.qqmusic.api.demo.R
-import com.tencent.qqmusic.api.demo.VisualActivity
+import com.tencent.qqmusic.api.demo.*
 import com.tencent.qqmusic.api.demo.openid.OpenIDHelper
-import com.tencent.qqmusic.api.demo.toPrintableString
 import com.tencent.qqmusic.api.demo.util.QPlayBindHelper
 import com.tencent.qqmusic.third.api.contract.*
-import com.tencent.qqmusic.third.api.contract.Events.API_EVENT_PLAY_SONG_CHANGED
-import com.tencent.qqmusic.third.api.contract.Keys.API_PARAM_KEY_MEDIA_INFO
-import com.tencent.qqmusic.third.api.contract.Keys.API_PARAM_KEY_PCM_FILE_DESCRIPTOR
 
-@Deprecated("remove later")
-class PcmExampleActivity : AppCompatActivity() {
+/**
+ * Created by clydeazhang on 2021/5/11 6:43 PM.
+ * Copyright (c) 2021 Tencent. All rights reserved.
+ */
+class PlayService : Service() {
+
     companion object {
-        const val TAG = "PcmExampleActivity"
+        private const val TAG = "PlayService"
     }
-
-    private lateinit var informationTextView: TextView
 
     private val qPlayBindHelper = QPlayBindHelper(this)
     var qqMusicApi: IQQMusicApi? = null
     private var curPlayState: Int = 0
+    private val handler = Handler()
 
     /**
      * QQ音乐事件回调
@@ -41,8 +38,7 @@ class PcmExampleActivity : AppCompatActivity() {
     private val eventListener = object : IQQMusicApiEventListener.Stub() {
         override fun onEvent(event: String, extra: Bundle) {
             Log.d(TAG, "onEvent:$event extra:${extra.toPrintableString()}")
-
-            runOnUiThread {
+            handler.post {
                 when (event) {
                     Events.API_EVENT_PLAY_STATE_CHANGED -> {
                         curPlayState = extra.getInt(Keys.API_EVENT_KEY_PLAY_STATE)
@@ -57,49 +53,55 @@ class PcmExampleActivity : AppCompatActivity() {
             val ret = p1?.extras?.get("ret")
             if (ret == "0") {
                 Log.d(TAG, "activeBroadcastReceiver 授权成功")
-                Toast.makeText(this@PcmExampleActivity, "授权成功", Toast.LENGTH_SHORT).show()
-                qqMusicApi?.registerEventListener(arrayListOf(Events.API_EVENT_PLAY_STATE_CHANGED, API_EVENT_PLAY_SONG_CHANGED), eventListener)
+                Toast.makeText(this@PlayService, "授权成功", Toast.LENGTH_SHORT).show()
+                qqMusicApi?.registerEventListener(arrayListOf(Events.API_EVENT_PLAY_STATE_CHANGED, Events.API_EVENT_PLAY_SONG_CHANGED), eventListener)
             } else {
                 Log.d(TAG, "activeBroadcastReceiver 授权失败($ret)")
             }
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pcm_example)
-
-        informationTextView = findViewById(R.id.tv_information)
-
-        findViewById<Button>(R.id.bt_bind_service).setOnClickListener {
-            bindService()
-        }
-
-        findViewById<Button>(R.id.bt_start_pcm_mode).setOnClickListener {
-            startPcmMode()
-        }
-
-        findViewById<Button>(R.id.bt_stop_pcm_mode).setOnClickListener {
-            stopPcmMode()
-        }
-
-        findViewById<Button>(R.id.bt_play).setOnClickListener {
-            resumeOrPause()
-        }
-
-        findViewById<Button>(R.id.bt_next).setOnClickListener {
-            playNext()
-        }
-
-        AudioTrackManager.printMessageCallback = {
-            runOnUiThread {
-                informationTextView.text = it
-            }
-        }
-
+    override fun onCreate() {
+        super.onCreate()
         val filter = IntentFilter()
         filter.addAction("callback_verify_notify")
         registerReceiver(activeBroadcastReceiver, filter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(activeBroadcastReceiver)
+        AudioTrackManager.stopPlay()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return object : IPlayService.Stub() {
+            override fun bindService() {
+                this@PlayService.bindService()
+            }
+
+            override fun startPcmMode() {
+                this@PlayService.startPcmMode()
+            }
+
+            override fun stopPcmMode() {
+                this@PlayService.stopPcmMode()
+            }
+
+            override fun resumeOrPause() {
+                this@PlayService.resumeOrPause()
+            }
+
+            override fun playNext() {
+                this@PlayService.playNext()
+            }
+
+            override fun setPrintMessageCallback(callback: IPrint?) {
+                AudioTrackManager.printMessageCallback = {
+                    callback?.print(it)
+                }
+            }
+        }
     }
 
     private fun playNext() {
@@ -158,12 +160,12 @@ class PcmExampleActivity : AppCompatActivity() {
                     return
                 }
 
-                val mediaInfo = registerResult?.getParcelable<Data.MediaInfo>(API_PARAM_KEY_MEDIA_INFO)
+                val mediaInfo = registerResult?.getParcelable<Data.MediaInfo>(Keys.API_PARAM_KEY_MEDIA_INFO)
                 mediaInfo ?: return
                 Log.d(TAG, "initData")
                 AudioTrackManager.initData(mediaInfo.sampleRateInHz, if (mediaInfo.channelConfig == 2) AudioFormat.CHANNEL_IN_STEREO else AudioFormat.CHANNEL_IN_MONO, mediaInfo.audioFormat)
 
-                val parcelFileDescriptor = registerResult.getParcelable<ParcelFileDescriptor>(API_PARAM_KEY_PCM_FILE_DESCRIPTOR)
+                val parcelFileDescriptor = registerResult.getParcelable<ParcelFileDescriptor>(Keys.API_PARAM_KEY_PCM_FILE_DESCRIPTOR)
                 parcelFileDescriptor ?: return
                 AudioTrackManager.startPlayByFileDescriptor(parcelFileDescriptor)
             }
@@ -188,17 +190,11 @@ class PcmExampleActivity : AppCompatActivity() {
                     val encryptString = OpenIDHelper.getEncryptString(nonce)
                     CommonCmd.verifyCallerIdentity(this, Config.OPENID_APPID, packageName, encryptString, "qqmusicapidemo://")
                 } else {
-                    Toast.makeText(this@PcmExampleActivity, "授权成功", Toast.LENGTH_SHORT).show()
-                    qqMusicApi?.registerEventListener(arrayListOf(Events.API_EVENT_PLAY_STATE_CHANGED, API_EVENT_PLAY_SONG_CHANGED), eventListener)
+                    Toast.makeText(this@PlayService, "授权成功", Toast.LENGTH_SHORT).show()
+                    qqMusicApi?.registerEventListener(arrayListOf(Events.API_EVENT_PLAY_STATE_CHANGED, Events.API_EVENT_PLAY_SONG_CHANGED), eventListener)
                 }
             }
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        AudioTrackManager.stopPlay()
-        unregisterReceiver(activeBroadcastReceiver)
     }
 
     private fun isPlaying(): Boolean {
