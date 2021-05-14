b3e89fab5ea2ad92b324c2b183903043b65d89f5
clydeazhang <clydeazhang@tencent.com>
2021-05-14 10:28:22 +0800

    fix compile error

--- demo/src/main/java/com/tencent/qqmusic/api/demo/ExampleListActivity.kt
+++ demo/src/main/java/com/tencent/qqmusic/api/demo/ExampleListActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import com.tencent.qqmusic.api.demo.pcm.PcmExampleActivity
 import com.tencent.qqmusic.api.demo.pcm.PcmExampleActivityNew
 import kotlinx.android.synthetic.main.activity_example_list.*
 
